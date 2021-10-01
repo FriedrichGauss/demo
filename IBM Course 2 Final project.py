dbbd7a8df1ebe48dafd0cd34c613009507c4ea24
FriedrichGauss <91580514+FriedrichGauss@users.noreply.github.com>
2021-09-30 22:02:25 -0500

    Update IBM Course 2 Final project.py
    Created a formula for the quadratic equation

--- IBM Course 2 Final project.py
+++ IBM Course 2 Final project.py
@@ -1,1 +1,10 @@
-
+# My Jupyter Notebook on IBM Watson Studio
+### Markus Bunge
+100% Human
+*I am interested in data science because I love to predict outcomes and learn how models operate*
+import numpy as np
+import math
+def quadratic(a,b,c):
+    pm = np.array([+1,-1])
+    return (-b+pm*math.sqrt(b**2-4*a*c))/(2*a)
+  quadratic(1,-4,-5)
